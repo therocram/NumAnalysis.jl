c0fc1324108636751f3cefafa67a31f61edb8b28
Masen <therocram@gmail.com>
2024-06-19 23:40:07 -0600

    Updated documentation for IVP methods

--- src/init_val_ode.jl
+++ src/init_val_ode.jl
@@ -69,34 +69,76 @@ function ivpsetup(a, b, y0, N, y=nothing, printres=false)
     return h, t, w, token2
 end
 
+# Add this to documentation soon
+# See also [`rungekuttaO4`](@ref), [`euler`](@ref), [`midpoint`](@ref)
+
 """
     ivpsolve(f, a, b, y0, N; solver=rungekuttaO4, y=nothing, printres=false)
 
 Approximate the solution ``y(t)`` to the initial-value problem
 ```math
-y' = f(t,y), \\hspace{0.2cm} a \\leq t \\leq b, \\hspace{0.2cm} y(a) = y0
+y' = f(t,y), \\hspace{0.2cm} a \\leq t \\leq b, \\hspace{0.2cm} y(a) = y_0
 ```
 at `N` + 1 equally spaced values of ``t`` on the interval [`a`,`b`] using the
 one step solver method given by `solver`.
 
-The function returns a tuple of the form `(t, w)`, where `t` contains all of the...
+The function returns a tuple of the form `(t, w)`, where `t` and `w` are Vectors containing all of the 
+independent and dependent variable values in the solution set, respectively. By default, 
+the Runge-Kutta method of order 4 (RKO4) is used:
 
 ```jldoctest
+julia> f(t,y) = -2t*y
+f (generic function with 1 method)
 
+julia> ivpsolve(f, 0, 1, 1, 5)
+([0.0, 0.2, 0.4, 0.6000000000000001, 0.8, 1.0], [1.0, 0.9607893333333333, 0.8521429680674133, 0.6976755803411453, 0.527297771054652, 0.3679036697909508])
 ```
 
 The user can optionally have the method print the results of each step of the solver by 
-setting `printres = true`:
+setting `printres = true`. Note that in the example below the actual function output is suppressed
+by the `;`:
 
 ```jldoctest
-
+julia> ivpsolve(f, 0, 1, 1, 5, printres=true);
+i   t_i        w_i
+0   0.000000   1.000000
+1   0.200000   0.960789
+2   0.400000   0.852143
+3   0.600000   0.697676
+4   0.800000   0.527298
+5   1.000000   0.367904
 ```
 
 Providing the true solution `y` will automatically print the results of each step along
 with the true error of the approximation at each step:
 
 ```jldoctest
+julia> y(t) = exp(-t^2)
+y (generic function with 1 method)
 
+julia> ivpsolve(f, 0, 1, 1, 5, y=y);
+i   t_i        w_i        |y(t_i) - w_i|
+0   0.000000   1.000000   0.000000
+1   0.200000   0.960789   0.000000
+2   0.400000   0.852143   0.000001
+3   0.600000   0.697676   0.000001
+4   0.800000   0.527298   0.000005
+5   1.000000   0.367904   0.000024
+```
+
+In addition to RKO4 there are two other single-step solver methods (Euler's method and 
+the Midpoint/Runge-Kutta Order 2 method). Out of the three, ROK4 will generally be the most
+accurate. Using Euler's Method, for example, will almost always give far less accurate results:
+
+```jldoctest
+julia> ivpsolve(f, 0, 1, 1, 5, solver=euler, y=y);
+i   t_i        w_i        |y(t_i) - w_i|
+0   0.000000   1.000000   0.000000
+1   0.200000   1.000000   0.039211
+2   0.400000   0.920000   0.067856
+3   0.600000   0.772800   0.075124
+4   0.800000   0.587328   0.060036
+5   1.000000   0.399383   0.031504
 ```
 """
 function ivpsolve(f, a, b, y0, N; solver=rungekuttaO4, y=nothing, printres=false)
